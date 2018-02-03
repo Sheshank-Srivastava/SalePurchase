61b5b0a20701e067bde8bf0a7cdea64600009293
Sheshank-Srivastava <ambitious9350164795@gmail.com>
2018-02-03 13:17:12 +0530

    Add action to the button and change text

--- saleApplication/src/saleapplication/DeleteItem.java
+++ saleApplication/src/saleapplication/DeleteItem.java
@@ -5,6 +5,8 @@
  */
 package saleapplication;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 /**
@@ -16,10 +18,71 @@ public class DeleteItem extends JFrame{
     JLabel itemCode,itemName,rate,stockAvail,minLevel,maxLevel,preOrderLevel;
     JLabel itemNameT,rateT,stockAvailT,minLevelT,maxLevelT,preOrderLevelT;
     JTextField itemCodetf;
+    JButton action;
+    int xAxies =10,yAxies=10,click=0;
     public DeleteItem(){
+       yAxies+=50;
        itemCode =new JLabel("Item Code");
-       itemCode.setBounds(10, 10, 60, 30);
+       itemCode.setBounds(xAxies, yAxies, 60, 30);
        add(itemCode);
+       itemCodetf = new JTextField();
+       itemCodetf.setBounds(xAxies+100,yAxies, 100, 30);
+       add(itemCodetf);
+       action = new JButton("Show");
+       action.setBounds(xAxies+230, yAxies, 80, 30);
+       add(action);
+//------------------------------------------------------------------------------
+       yAxies+=50;
+       itemName =new JLabel("Item Name");
+       itemName.setBounds(xAxies, yAxies, 60, 30);
+       add(itemName);
+       itemNameT = new JLabel("--");
+       itemNameT.setBounds(xAxies+100,yAxies, 100, 30);
+       add(itemNameT);
+//-----------------------------------------------------------------------------       
+       yAxies+=50;
+       rate =new JLabel("Rate");
+       rate.setBounds(xAxies, yAxies, 60, 30);
+       add(rate);
+       rateT = new JLabel("--");
+       rateT.setBounds(xAxies+100,yAxies, 100, 30);
+       add(rateT);
+//-----------------------------------------------------------------------------       
+       yAxies+=50;
+       stockAvail =new JLabel("Stock Available");
+       stockAvail.setBounds(xAxies, yAxies, 100, 30);
+       add(stockAvail);
+       stockAvailT = new JLabel("--");
+       stockAvailT.setBounds(xAxies+100,yAxies, 100, 30);
+       add(stockAvailT);
+//-----------------------------------------------------------------------------       
+       yAxies+=50;
+       minLevel =new JLabel("Min Level");
+       minLevel.setBounds(xAxies, yAxies, 60, 30);
+       add(minLevel);
+       minLevelT = new JLabel("--");
+       minLevelT.setBounds(xAxies+100,yAxies, 100, 30);
+       add(minLevelT);
+       /**
+        * Action to the Button is Defined here
+        * On First click All the Remaining Fields Are Filled from the database
+        * And change the button Text to Delete
+        */
+       action.addActionListener(new ActionListener(){
+
+           @Override
+           public void actionPerformed(ActionEvent ae) {
+               click++;
+               if(click==1){
+                   action.setText("Delete");
+                   
+               }
+               if(click==2){
+                  JOptionPane.showConfirmDialog(action, "Confirm Want to Delete");
+               }
+           }
+           
+       });
        /**
         * Frame Attributes
         */
