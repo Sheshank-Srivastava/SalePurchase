f8fc9862addc645fe706e3698c647eab51c861c7
Sheshank-Srivastava <ambitious9350164795@gmail.com>
2018-02-04 17:17:59 +0530

    Delete Item Frame Works Complete

--- saleApplication/src/saleapplication/DeleteItem.java
+++ saleApplication/src/saleapplication/DeleteItem.java
@@ -7,6 +7,9 @@ package saleapplication;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.*;
 
 /**
@@ -19,7 +22,11 @@ public class DeleteItem extends JFrame{
     JLabel itemNameT,rateT,stockAvailT,minLevelT,maxLevelT,preOrderLevelT;
     JTextField itemCodetf;
     JButton action;
+    String query=null;
+    
     int xAxies =10,yAxies=10,click=0;
+    MakeConnection mc = new MakeConnection();
+       Connection con;
     public DeleteItem(){
         yAxies+=50;
         itemCode =new JLabel("Item Code");
@@ -36,7 +43,7 @@ public class DeleteItem extends JFrame{
         itemName =new JLabel("Item Name");
         itemName.setBounds(xAxies, yAxies, 60, 30);
         add(itemName);
-        itemNameT = new JLabel("--");
+        itemNameT = new JLabel();
         itemNameT.setBounds(xAxies+100,yAxies, 100, 30);
         add(itemNameT);
 //-----------------------------------------------------------------------------
@@ -44,7 +51,7 @@ public class DeleteItem extends JFrame{
         rate =new JLabel("Rate");
         rate.setBounds(xAxies, yAxies, 60, 30);
         add(rate);
-        rateT = new JLabel("--");
+        rateT = new JLabel();
         rateT.setBounds(xAxies+100,yAxies, 100, 30);
         add(rateT);
 //-----------------------------------------------------------------------------
@@ -52,7 +59,7 @@ public class DeleteItem extends JFrame{
         stockAvail =new JLabel("Stock Available");
         stockAvail.setBounds(xAxies, yAxies, 100, 30);
         add(stockAvail);
-        stockAvailT = new JLabel("--");
+        stockAvailT = new JLabel();
         stockAvailT.setBounds(xAxies+100,yAxies, 100, 30);
         add(stockAvailT);
 //-----------------------------------------------------------------------------
@@ -60,7 +67,7 @@ public class DeleteItem extends JFrame{
         minLevel =new JLabel("Min Level");
         minLevel.setBounds(xAxies, yAxies, 60, 30);
         add(minLevel);
-        minLevelT = new JLabel("--");
+        minLevelT = new JLabel();
         minLevelT.setBounds(xAxies+100,yAxies, 100, 30);
         add(minLevelT);
 //-----------------------------------------------------------------------------
@@ -68,7 +75,7 @@ public class DeleteItem extends JFrame{
         maxLevel =new JLabel("Max Level");
         maxLevel.setBounds(xAxies, yAxies, 60, 30);
         add(maxLevel);
-        maxLevelT = new JLabel("--");
+        maxLevelT = new JLabel();
         maxLevelT.setBounds(xAxies+100,yAxies, 100, 30);
         add(maxLevelT);
 //-----------------------------------------------------------------------------
@@ -76,7 +83,7 @@ public class DeleteItem extends JFrame{
         preOrderLevel =new JLabel("Pre-Order Level");
         preOrderLevel.setBounds(xAxies, yAxies, 100, 30);
         add(preOrderLevel);
-        preOrderLevelT = new JLabel("--");
+        preOrderLevelT = new JLabel();
         preOrderLevelT.setBounds(xAxies+100,yAxies, 100, 30);
         add(preOrderLevelT);
         /**
@@ -89,17 +96,64 @@ public class DeleteItem extends JFrame{
             @Override
             public void actionPerformed(ActionEvent ae) {
                 click++;
+                int p=0;
                 if(click==1){
-                    try{
-                                                
-                    }catch(Exception e){
+                    query ="select itemName,rate,stockAvail,minLevel,maxLevel,preOrderLevel from salepurchase.itemdetail where itemCode=?";
+                    System.out.println(itemCodetf.getText());
+                    if(!"".equals(itemCodetf.getText())){
+                        p=1;
+                        System.out.println(p);
+                    }
+                    if(p==1){
+                        try{
+                            System.out.println("in execution");
+                            con = mc.getConnection();
+                            PreparedStatement ps = con.prepareStatement(query);
+                            ps.setString(1, itemCodetf.getText());
+                            ResultSet rs = ps.executeQuery();
+                            
+                            if(rs.next()){
+                                System.out.println("hola");
+                                itemNameT.setText(rs.getString(1));
+                                rateT.setText(rs.getString(2));
+                                stockAvailT.setText(rs.getString(3));
+                                minLevelT.setText(rs.getString(4));
+                                maxLevelT.setText(rs.getString(5));
+                                preOrderLevelT.setText(rs.getString(6));
+                                action.setText("Delete");
+                            }
+                            else{
+                                click=0;
+                                JOptionPane.showMessageDialog(action, "Item Doesn't Exsist");
+                            }
+                        } catch (ClassNotFoundException | SQLException ex) {
+                            Logger.getLogger(DeleteItem.class.getName()).log(Level.SEVERE, null, ex);
+                        }
                         
                     }
-                    action.setText("Delete");
-                    
+                    else{
+                        click--;
+                        JOptionPane.showMessageDialog(action, "Fill the Item Code");
+                    }
                 }
                 if(click==2){
-                    JOptionPane.showConfirmDialog(action, "Confirm Want to Delete");
+                    int x= JOptionPane.showConfirmDialog(action, "Confirm Want to Delete");
+                    query ="delete from itemdetail where itemCode=?";
+                    if(x==0){
+                        try{
+                            con = mc.getConnection();
+                            PreparedStatement ps =con.prepareStatement(query);
+                            ps.setString(1, itemCodetf.getText());
+                            p= ps.executeUpdate();
+                            if(p==1){
+                                JOptionPane.showMessageDialog(action,"data is deleted");
+                              
+                                click=0;
+                            }
+                        } catch (SQLException | ClassNotFoundException ex) {
+                            Logger.getLogger(DeleteItem.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
                 }
             }
             
