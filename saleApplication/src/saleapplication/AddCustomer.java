d5d5d5e4aecedca2f5abeeedad7bad19818def01
Sheshank-Srivastava <ambitious9350164795@gmail.com>
2018-02-04 09:28:33 +0530

    Completed the Work of Add Customer and it auto generate code for new Cutomer

--- saleApplication/src/saleapplication/AddCustomer.java
+++ saleApplication/src/saleapplication/AddCustomer.java
@@ -1,14 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package saleapplication;
 
+import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -21,34 +24,35 @@ import javax.swing.JTextField;
  * @author TANSU
  */
 public class AddCustomer  extends JFrame{
-    JLabel custCode,custName,custAdd,custCity,custState,custPin,custStatus;
-    JTextField custCodetf,custNametf,custAddtf,custStatetf,custCitytf,custPintf;
+    JLabel custCode,custName,custAdd,custCity,custState,custPin,custStatus,custCodetf;
+    JTextField custNametf,custAddtf,custStatetf,custCitytf,custPintf;
     JComboBox status;
     JButton saveb;
     int xAxies=10,yAxies=30;
     String sta[] ={"Excellent","Good","Annoying"};
-      MakeConnection mc = new MakeConnection();
+    String query;
+    MakeConnection mc = new MakeConnection();
     Connection con;
     public AddCustomer(){
         yAxies+=20;
         custCode = new JLabel("Customer Id");
         custCode.setBounds(xAxies, yAxies, 120, 30);
-        add(custCode);       
-        custCodetf = new JTextField();
+        add(custCode);
+        custCodetf = new JLabel("--");
         custCodetf.setBounds(xAxies+130, yAxies, 120, 30);
         add(custCodetf);
         yAxies+=40;
         custName = new JLabel("Customer Name");
         custName.setBounds(xAxies, yAxies, 120, 30);
-        add(custName);       
+        add(custName);
         custNametf = new JTextField();
         custNametf.setBounds(xAxies+130, yAxies, 120, 30);
         add(custNametf);
-       
+        
         yAxies+=40;
         custAdd = new JLabel("Customer Address");
         custAdd.setBounds(xAxies, yAxies, 120, 30);
-        add(custAdd);       
+        add(custAdd);
         custAddtf = new JTextField();
         custAddtf.setBounds(xAxies+130, yAxies, 120, 30);
         add(custAddtf);
@@ -56,7 +60,7 @@ public class AddCustomer  extends JFrame{
         yAxies+=40;
         custState = new JLabel("State");
         custState.setBounds(xAxies, yAxies, 120, 30);
-        add(custState);       
+        add(custState);
         custStatetf = new JTextField();
         custStatetf.setBounds(xAxies+130, yAxies, 120, 30);
         add(custStatetf);
@@ -64,7 +68,7 @@ public class AddCustomer  extends JFrame{
         yAxies+=40;
         custCity = new JLabel("City");
         custCity.setBounds(xAxies, yAxies, 120, 30);
-        add(custCity);       
+        add(custCity);
         custCitytf = new JTextField();
         custCitytf.setBounds(xAxies+130, yAxies, 120, 30);
         add(custCitytf);
@@ -80,7 +84,7 @@ public class AddCustomer  extends JFrame{
         yAxies+=40;
         custStatus= new JLabel("City");
         custStatus.setBounds(xAxies, yAxies, 120, 30);
-        add(custStatus);       
+        add(custStatus);
         status = new JComboBox(sta);
         status.setBounds(xAxies+130, yAxies, 120, 30);
         add(status);
@@ -89,13 +93,36 @@ public class AddCustomer  extends JFrame{
         saveb = new JButton("Save");
         saveb.setBounds(xAxies,yAxies,100,30);
         add(saveb);
+        
+        /**
+         * Genetrating Customer Code
+         */
+        try{
+            String custCode = null;
+            query="select custCode from customer";
+            con = mc.getConnection();
+            PreparedStatement ps= con.prepareStatement(query);
+            ResultSet rs= ps.executeQuery();
+            while(rs.next()){
+                custCode =rs.getString(1);
+            }
+            int newCode = Integer.parseInt(custCode)+1;
+            custCodetf.setText(Integer.toString(newCode));
+        }catch(ClassNotFoundException | SQLException e){
+            
+        }
+        /**
+         * Define Action to the save Button
+         */
         saveb.addActionListener(new ActionListener(){
-
+            
             @Override
             public void actionPerformed(ActionEvent ae) {
                 try{
-                    String query ="insert into customer values(?,?,?,?,?,?,?)";
-                    PreparedStatement ps =con.prepareStatement(query);
+                    System.out.println("hello");
+                    query ="insert into customer values(?,?,?,?,?,?,?)";
+                    con = mc.getConnection();
+                    PreparedStatement ps = con.prepareStatement(query);
                     ps.setString(1,custCodetf.getText());
                     ps.setString(2, custNametf.getText());
                     ps.setString(3, custAddtf.getText());
@@ -108,10 +135,12 @@ public class AddCustomer  extends JFrame{
                         JOptionPane.showMessageDialog(saveb, "update Successfull");
                     }
                     else{
-                         JOptionPane.showMessageDialog(saveb, "update unSuccessfull");
+                        JOptionPane.showMessageDialog(saveb, "update unSuccessfull");
                     }
-                }catch(Exception e){
+                }catch(SQLException | HeadlessException e){
                     System.out.println(e);
+                } catch (ClassNotFoundException ex) {
+                    Logger.getLogger(AddCustomer.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
         });
